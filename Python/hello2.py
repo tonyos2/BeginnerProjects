a4ce2d7e1812d2d9ead2dd3e0243e0b3c5e2b19b
tonyos2 <35715965+tonyos2@users.noreply.github.com>
2023-04-25 22:02:54 -0700

    Hello world, simple arithmetic, arrays

---
+++ Python/hello2.py
@@ -0,0 +1,19 @@
+# Print results between two numbers
+x = 6
+y = 2
+
+# Arithmetic Operations
+summation = x + y
+subtract = x - y
+multiply = x * y
+division = x / y
+modulo = x % y
+exponent = x ** y
+
+# Print results
+print(summation)
+print(subtract)
+print(multiply)
+print(division)
+print(modulo)
+print(exponent)
---
+++ Python/hello3.py
@@ -0,0 +1,19 @@
+fruits = ["apples", "bananas", "cherries"]
+animals = ["jaguar", "cobra", "monkey", "gorilla", "elephant", "lion", "tiger"]
+
+for fruit in fruits:
+    print(fruit)
+
+print()
+
+for animal in animals:
+    print(animal)
+
+print()
+
+tz = 0
+values = [1, 3, 5, 7, 9] # sum is 10 + 10 + 5 = 25
+for value in values:
+    tz += value
+
+print(tz)
